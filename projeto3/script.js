1e04cd00ff779eaa999c6a82b85ae08ea6bf3b0f
Leandro Ferreira <leandroferreira@live.com>
2026-04-09 15:21:35 -0300

    Atualizando projeto

---
+++ projeto3/script.js
@@ -0,0 +1,20 @@
+const celulas = document.querySelectorAll('.celula'); 
+
+let vezDoX = true;
+
+document.getElementById("botaoReiniciar").addEventListener('click',iniciarJogo);
+
+function iniciarJogo(){
+    vezDoX = true;
+    celulas.forEach(celula =>{
+        celula.textContent = "";
+        celula.addEventListener('click', tratarClique, {once:true});
+    });
+}
+
+function tratarClique(evento){
+    evento.target.textContent = vezDoX ? "X" : "O";
+    vezDoX = !vezDoX;
+}
+
+iniciarJogo();
---
+++ script.js
@@ -0,0 +1,15 @@
+function abrirProjeto1() {
+  window.open("projeto1/index.html", "_blank");
+}
+
+function abrirProjeto2() {
+  window.open("projeto2/index.html", "_blank");
+}
+
+function abrirProjeto3() {
+  window.open("projeto3/index.html", "_blank");
+}
+
+function abrirProjeto4() {
+  window.open("projeto4/index.html", "_blank");
+}
